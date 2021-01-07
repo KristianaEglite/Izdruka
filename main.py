66fdffc4d2f4499c3decb9877e71d0ad30fce5a0
KristianaEglite <groboleva@gmail.com>
2021-01-07 10:35:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello, world!")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons") 
+print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.") #kombinētā izdruka - teksts un aprēķins
+print("Kristiāna " *5)
+print(f"Šajā gadā būs {365*60*60*24} sekundes.")
+print(0.1+0.2-0.3)
