8b63addd27718f4f171c0c5acdc23c4e23b785d6
Kristiāna <groboleva@gmail.com>
2021-01-07 10:39:39 +0000

    Samainīju pitons uz python

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print("Hello, world!")
 print(2 * 2)
-print(2 * 2, 2 * 3, 2 * 4, "Pitons") 
+print(2 * 2, 2 * 3, 2 * 4, "Python") 
 print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.") #kombinētā izdruka - teksts un aprēķins
+print("sveika, " + "pasaule!")
 print("Kristiāna " *5)
 print(f"Šajā gadā būs {365*60*60*24} sekundes.")
 print(0.1+0.2-0.3)
